9c6a13587f840f2b71dfbc88c88e54de9c56f327
rishuKumar4you <rishukumarforyou@gmail.com>
2025-10-23 21:59:16 +0530

    Implement EntitySearch and EntityPagination components in workflows, enhancing search and pagination functionality. Fix typo in useWorkflowsParams hook. Update workflows container to integrate new components for improved user experience.

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -1,110 +1,109 @@
-import { createTRPCRouter, protectedProcedure,premiumProcedure } from "@/trpc/init";
-import prisma from "@/lib/db";
-import { generateSlug } from "random-word-slugs";
-import z from "zod";
-import { PAGINATION } from "@/config/constants";
+import {PAGINATION} from '@/config/constants';
+import prisma from '@/lib/db';
+import {createTRPCRouter, premiumProcedure, protectedProcedure} from '@/trpc/init';
+import {generateSlug} from 'random-word-slugs';
+import z from 'zod';
 
 export const workflowsRouter = createTRPCRouter({
-  create:
-      premiumProcedure.mutation(({ctx}) => {
-        return prisma.workflow.create({
-            data: {
-                name: generateSlug(3),
-                userId: ctx.auth.user.id,
-            },
-        });
-      }),
+  create: premiumProcedure.mutation(({ctx}) => {
+    return prisma.workflow.create({
+      data: {
+        name: generateSlug(3),
+        userId: ctx.auth.user.id,
+      },
+    });
+  }),
 
-  remove:
-      protectedProcedure
-          .input(z.object({
-            id: z.string()
-          }))  // attempt to delete the workflow using this id and the currently
-               // logged in user
-          .mutation(({ctx, input}) => {
-            return prisma.workflow.delete({
-                where: {
+  remove: protectedProcedure
+              .input(z.object({
+                id: z.string()
+              }))  // attempt to delete the workflow using this id and the
+                   // currently logged in user
+              .mutation(({ctx, input}) => {
+                return prisma.workflow.delete({
+                  where: {
                     id: input.id,
                     userId: ctx.auth.user.id,
-                },
-            });
-          }),
+                  },
+                });
+              }),
 
-  updateName:
-      protectedProcedure
-          .input(z.object({id: z.string(), name: z.string().min(1)}))
-          .mutation(({ctx, input}) => {
-            return prisma.workflow.update({
-                where: {
-                    id: input.id,
-                    userId: ctx.auth.user.id,
-                },
-                data: {
-                    name: input.name
-                },
-            });
-          }),
+  updateName: protectedProcedure
+                  .input(z.object({id: z.string(), name: z.string().min(1)}))
+                  .mutation(({ctx, input}) => {
+                    return prisma.workflow.update({
+                      where: {
+                        id: input.id,
+                        userId: ctx.auth.user.id,
+                      },
+                      data: {name: input.name},
+                    });
+                  }),
   getOne: protectedProcedure.input(z.object({id: z.string()}))
               .query(({ctx, input}) => {
                 return prisma.workflow.findUnique({
-                    where: {
-                        id: input.id,
-                        userId: ctx.auth.user.id,
-                    }
+                  where: {
+                    id: input.id,
+                    userId: ctx.auth.user.id,
+                  }
                 });
               }),
-  getMany: protectedProcedure   // fetch all of the workflows of the user 
-    .input(z.object({
-      page: z.number().default(PAGINATION.DEFAULT_PAGE),
-      pageSize: z
-        .number()
-        .min(PAGINATION.MIN_PAGE_SIZE)
-        .max(PAGINATION.MAX_PAGE_SIZE)
-        .default(PAGINATION.DEFAULT_PAGE_SIZE),
-      search: z.string().default(""),
-                }))
-                .query(async({ ctx, input }) => {
-                  const { page, pageSize, search } = input;
+  getMany: protectedProcedure  // fetch all of the workflows of the user
+               .input(z.object({
+                 page: z.number().default(PAGINATION.DEFAULT_PAGE),
+                 pageSize: z.number()
+                               .min(PAGINATION.MIN_PAGE_SIZE)
+                               .max(PAGINATION.MAX_PAGE_SIZE)
+                               .default(PAGINATION.DEFAULT_PAGE_SIZE),
+                 search: z.string().default(''),
+               }))
+               .query(async ({ctx, input}) => {
+                 const {page, pageSize, search} = input;
 
-                  const [items, totalCount] = await Promise.all([
-                    prisma.workflow.findMany({
-                      skip: (page - 1) * pageSize,
-                      take: pageSize,
+                 const [items, totalCount] = await Promise.all([
+                   prisma.workflow.findMany({
+                     skip: (page - 1) * pageSize,
+                     take: pageSize,
 
-                      where: {
-                        userId: ctx.auth.user.id,
-                        name: {
-                          contains: search,
-                          mode:"insensitive",
-                        },
-                      },
-                      orderBy: {
-                        updatedAt: "desc",
-                      },
+                     where: {
+                       userId: ctx.auth.user.id,
+                       name: {
+                         contains: search,
+                         mode: 'insensitive',
+                       },
+                     },
+                     orderBy: {
+                       updatedAt: 'desc',
+                     },
 
-                    }),
-                    prisma.workflow.count({
-                      where: {
-                        userId: ctx.auth.user.id,
-                      },
-                    }),
-                  ]);
-                  const totalPages = Math.ceil(totalCount / pageSize);
-                  const hasNextPage = page < totalPages;
-                  const hasPreviousPage = page > 1;
-                  
-                  return {
-                    items,
-                    page,
-                    pageSize,
-                    totalCount,
-                    totalPages,
-                    hasNextPage,
-                    hasPreviousPage,
-                  };
-                }),
+                   }),
+                   prisma.workflow.count({
+                     where: {
+                       userId: ctx.auth.user.id,
+                       name: {
+                         contains: search,
+                         mode: 'insensitive',
+                       },
+                     },
+                   }),
+                 ]);
+                 const totalPages = Math.ceil(totalCount / pageSize);
+                 const hasNextPage = page < totalPages;
+                 const hasPreviousPage = page > 1;
+
+                 return {
+                   items,
+                   page,
+                   pageSize,
+                   totalCount,
+                   totalPages,
+                   hasNextPage,
+                   hasPreviousPage,
+                 };
+               }),
 
 });
 
-// create a protected procedure for creating a workflow. 
-// we are adding CRUD operation for workflows, with a rondom name and authenticated user id, 
+// create a protected procedure for creating a workflow.
+// we are adding CRUD operation for workflows, with a rondom name and
+// authenticated user id,
